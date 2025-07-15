2fa24bd2b058216f39ba81964c3ff69a3ea87cb0
Kartikc17 <kartikchalamarad17@gmail.com>
2025-07-15 21:02:25 +0530

    initial commit

---
+++ src/main/java/com/salesSavvy/repository/OrderRepository.java
@@ -0,0 +1,6 @@
+package com.salesSavvy.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.salesSavvy.entity.Orders;
+public interface OrderRepository extends JpaRepository<Orders, String>{
+	
+}
